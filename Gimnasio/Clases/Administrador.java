ef7bd350bbd1f6b28bcf77b0a3daaa8fb1fd1d5c
Val <171977808+imquesitoO@users.noreply.github.com>
2024-10-27 14:43:26 -0500

    Administrador.java

--- Gimnasio/Clases/Administrador.java
+++ Gimnasio/Clases/Administrador.java
@@ -1,6 +1,7 @@
 package gimnasio;
 
 public class Administrador extends Persona {
+        private Gimnasio gimnasio
         private String usuario;
         private String contrasenia;
 
@@ -25,37 +26,13 @@ public class Administrador extends Persona {
         this.contrasenia = contrasenia;
     }
     
-    public void Mostrar_Alumno(String codigo){
-        String id=codigo;
-        if(id.equals( getCodigoGymrat())|| id.equals(getUsuarioGymRat())){
-            
-        }
+    public void Mostrar_Alumno(){
+        //mostrar alumno en especifico
     }
-    public void Mostrar_Datos(){
-        String [][] datos= {{"Usuario", usuario}, {"Nombre ",getNombres()}, { "Apellidos ",getApellidos()}, {"DNI ",String.valueOf(getDni())}, {"Edad", String.valueOf(getEdad())}};
-        imprimirTabla(datos);
+    public void Mostrar_Entrenadores(){
+        //mostrar todos los entrenadores
     }
-    
-    public static void imprimirTabla(String[][] datos) {
-        int anchoColumna1 = 20; // Ajusta según la longitud de los textos en la primera columna
-        int anchoColumna2 = 30; // Ajusta según la longitud de los textos en la segunda columna
-
-        // Imprime la línea superior de la tabla
-        imprimirLinea(anchoColumna1, anchoColumna2);
-
-        // Imprime cada fila de datos
-        for (String[] fila : datos) {
-            System.out.printf("| %-"+(anchoColumna1-2)+"s | %-"+(anchoColumna2-2)+"s |%n", fila[0], fila[1]);
-            imprimirLinea(anchoColumna1, anchoColumna2);
-        }
+    public void Mostrar_Membresias(){
+        //mostrar solo alumnos con membresia premium
     }
-
-    public static void imprimirLinea(int anchoColumna1, int anchoColumna2) {
-        System.out.print("+");
-        for (int i = 0; i < anchoColumna1; i++) System.out.print("-");
-        System.out.print("+");
-        for (int i = 0; i < anchoColumna2; i++) System.out.print("-");
-        System.out.println("+");
-    }
-    
 }
