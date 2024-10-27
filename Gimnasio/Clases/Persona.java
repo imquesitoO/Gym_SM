c50ac367b756d647325cc7769c7ed7becdb8188b
Val <171977808+imquesitoO@users.noreply.github.com>
2024-10-27 14:48:34 -0500

    Persona.java

--- Gimnasio/Clases/Persona.java
+++ Gimnasio/Clases/Persona.java
@@ -1,24 +1,24 @@
 package gimnasio;
 public class Persona {
-    protected  String nombres;
+    protected  String nombre;
     protected  String apellidos;
     protected  long dni;
     protected  int edad;
     protected  String genero;
 
 
-    public Persona(String nombres, String apellidos, long dni, int edad, String genero) {
-        this.nombres = nombres;
+    public Persona(String nombre, String apellidos, long dni, int edad, String genero) {
+        this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
         this.edad=edad;
         this.genero=genero; }
 
-    public String getNombres() {
+    public String getNombre() {
         return nombres;
     }
 
-    public void setNombres(String nombres) {
+    public void setNombre(String nombres) {
         this.nombres = nombres;
     }
 
